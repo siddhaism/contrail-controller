620a6eb9c22adc39fd37e9082d1123ea6e4199c8
Nischal Sheth <nsheth@juniper.net>
2014-02-10 23:59:59 -0800

    Checkpoint.

--- src/bgp/bgp_multicast.h
+++ src/bgp/bgp_multicast.h
@@ -125,9 +125,9 @@ struct McastForwarderCompare {
             return true;
         if (lhs->route_distinguisher() > rhs->route_distinguisher())
             return false;
-        if (lhs->router_id() < rhs->router_id())
+        if (lhs->router_id().to_ulong() < rhs->router_id().to_ulong())
             return true;
-        if (lhs->router_id() > rhs->router_id())
+        if (lhs->router_id().to_ulong() > rhs->router_id().to_ulong())
             return false;
 
         return false;
