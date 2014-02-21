73ccc130e997c750df34c636dbbcecae4f8923ae
Nischal Sheth <nsheth@juniper.net>
2014-02-21 14:21:39 -0800

    Checkpoint.

--- src/bgp/test/bgp_xmpp_mcast_test.cc
+++ src/bgp/test/bgp_xmpp_mcast_test.cc
@@ -128,13 +128,13 @@ protected:
 
     virtual void SessionUp() {
         agent_xa_.reset(new test::NetworkAgentMock(
-                &evm_, "agent-a", xs_x_->GetPort(), "127.0.0.1"));
+                &evm_, "agent-xa", xs_x_->GetPort(), "127.0.0.1", "127.0.0.101"));
         TASK_UTIL_EXPECT_TRUE(agent_xa_->IsEstablished());
         agent_xb_.reset(new test::NetworkAgentMock(
-                &evm_, "agent-b", xs_x_->GetPort(), "127.0.0.2"));
+                &evm_, "agent-xb", xs_x_->GetPort(), "127.0.0.2", "127.0.0.101"));
         TASK_UTIL_EXPECT_TRUE(agent_xb_->IsEstablished());
         agent_xc_.reset(new test::NetworkAgentMock(
-                &evm_, "agent-c", xs_x_->GetPort(), "127.0.0.3"));
+                &evm_, "agent-xc", xs_x_->GetPort(), "127.0.0.3", "127.0.0.101"));
         TASK_UTIL_EXPECT_TRUE(agent_xc_->IsEstablished());
     }
 
@@ -153,7 +153,7 @@ protected:
     }
 
     virtual void Configure(const char *config_tmpl) {
-        char config[4096];
+        char config[8192];
         snprintf(config, sizeof(config), config_tmpl,
                  bs_x_->session_manager()->GetPort());
         bs_x_->Configure(config);
@@ -219,8 +219,8 @@ int BgpXmppMcastTest::validate_done_;
 static const char *config_tmpl1 = "\
 <config>\
     <bgp-router name=\'X\'>\
-        <identifier>192.168.0.1</identifier>\
-        <address>127.0.0.1</address>\
+        <identifier>192.168.0.101</identifier>\
+        <address>127.0.0.101</address>\
         <port>%d</port>\
     </bgp-router>\
     <routing-instance name='blue'>\
@@ -994,8 +994,8 @@ TEST_F(BgpXmppMcastEncapTest, Change) {
 static const char *config_tmpl2 = "\
 <config>\
     <bgp-router name=\'X\'>\
-        <identifier>192.168.0.1</identifier>\
-        <address>127.0.0.1</address>\
+        <identifier>192.168.0.101</identifier>\
+        <address>127.0.0.101</address>\
         <port>%d</port>\
         <session to=\'Y\'>\
             <address-families>\
@@ -1004,8 +1004,8 @@ static const char *config_tmpl2 = "\
         </session>\
     </bgp-router>\
     <bgp-router name=\'Y\'>\
-        <identifier>192.168.0.4</identifier>\
-        <address>127.0.0.4</address>\
+        <identifier>192.168.0.102</identifier>\
+        <address>127.0.0.102</address>\
         <port>%d</port>\
         <session to=\'X\'>\
             <address-families>\
@@ -1025,7 +1025,7 @@ static const char *config_tmpl2 = "\
 class BgpXmppMcast2ServerTestBase : public BgpXmppMcastTest {
 protected:
     virtual void Configure(const char *config_tmpl) {
-        char config[4096];
+        char config[8192];
         snprintf(config, sizeof(config), config_tmpl,
                  bs_x_->session_manager()->GetPort(),
                  bs_y_->session_manager()->GetPort());
@@ -1063,13 +1063,13 @@ protected:
         BgpXmppMcastTest::SessionUp();
 
         agent_ya_.reset(new test::NetworkAgentMock(
-                &evm_, "agent-ya", xs_y_->GetPort(), "127.0.0.4", "127.0.0.4"));
+                &evm_, "agent-ya", xs_y_->GetPort(), "127.0.0.4", "127.0.0.102"));
         TASK_UTIL_EXPECT_TRUE(agent_ya_->IsEstablished());
         agent_yb_.reset(new test::NetworkAgentMock(
-                &evm_, "agent-yb", xs_y_->GetPort(), "127.0.0.5", "127.0.0.4"));
+                &evm_, "agent-yb", xs_y_->GetPort(), "127.0.0.5", "127.0.0.102"));
         TASK_UTIL_EXPECT_TRUE(agent_yb_->IsEstablished());
         agent_yc_.reset(new test::NetworkAgentMock(
-                &evm_, "agent-yc", xs_y_->GetPort(), "127.0.0.6", "127.0.0.4"));
+                &evm_, "agent-yc", xs_y_->GetPort(), "127.0.0.6", "127.0.0.102"));
         TASK_UTIL_EXPECT_TRUE(agent_yc_->IsEstablished());
     }
 
@@ -1274,6 +1274,191 @@ TEST_F(BgpXmppMcast2ServerTest, MultipleAgentPerServer3) {
     agent_yb_->DeleteMcastRoute("blue", mroute);
     agent_yc_->DeleteMcastRoute("blue", mroute);
     task_util::WaitForIdle();
+};
+
+static const char *config_tmpl3 = "\
+<config>\
+    <bgp-router name=\'X\'>\
+        <identifier>192.168.0.101</identifier>\
+        <address>127.0.0.101</address>\
+        <port>%d</port>\
+        <session to=\'Y\'>\
+            <address-families>\
+                <family>inet-mvpn</family>\
+            </address-families>\
+        </session>\
+        <session to=\'Z\'>\
+            <address-families>\
+                <family>inet-mvpn</family>\
+            </address-families>\
+        </session>\
+    </bgp-router>\
+    <bgp-router name=\'Y\'>\
+        <identifier>192.168.0.102</identifier>\
+        <address>127.0.0.102</address>\
+        <port>%d</port>\
+        <session to=\'X\'>\
+            <address-families>\
+                <family>inet-mvpn</family>\
+            </address-families>\
+        </session>\
+        <session to=\'Z\'>\
+            <address-families>\
+                <family>inet-mvpn</family>\
+            </address-families>\
+        </session>\
+    </bgp-router>\
+    <bgp-router name=\'Z\'>\
+        <identifier>192.168.0.103</identifier>\
+        <address>127.0.0.103</address>\
+        <port>%d</port>\
+        <session to=\'X\'>\
+            <address-families>\
+                <family>inet-mvpn</family>\
+            </address-families>\
+        </session>\
+        <session to=\'Y\'>\
+            <address-families>\
+                <family>inet-mvpn</family>\
+            </address-families>\
+        </session>\
+    </bgp-router>\
+    <routing-instance name='blue'>\
+        <vrf-target>target:1:1</vrf-target>\
+    </routing-instance>\
+    <routing-instance name='pink'>\
+        <vrf-target>target:1:2</vrf-target>\
+    </routing-instance>\
+</config>\
+";
+
+class BgpXmppMcast3ServerTestBase : public BgpXmppMcast2ServerTestBase {
+protected:
+    virtual void Configure(const char *config_tmpl) {
+        char config[8192];
+        snprintf(config, sizeof(config), config_tmpl,
+                 bs_x_->session_manager()->GetPort(),
+                 bs_y_->session_manager()->GetPort(),
+                 bs_z_->session_manager()->GetPort());
+        bs_x_->Configure(config);
+        bs_y_->Configure(config);
+        bs_z_->Configure(config);
+    }
+
+    virtual void SetUp() {
+        bs_z_.reset(new BgpServerTest(&evm_, "Z"));
+        xs_z_ = new XmppServer(&evm_, test::XmppDocumentMock::kControlNodeJID);
+        bs_z_->session_manager()->Initialize(0);
+        xs_z_->Initialize(0, false);
+        bcm_z_.reset(new BgpXmppChannelManagerMock(xs_z_, bs_z_.get()));
+
+        BgpXmppMcast2ServerTestBase::SetUp();
+    }
+
+    virtual void TearDown() {
+        xs_z_->Shutdown();
+        task_util::WaitForIdle();
+        bs_z_->Shutdown();
+        task_util::WaitForIdle();
+        bcm_z_.reset();
+        TcpServerManager::DeleteServer(xs_z_);
+        xs_z_ = NULL;
+
+        agent_za_->Delete();
+        agent_zb_->Delete();
+        agent_zc_->Delete();
+
+        BgpXmppMcast2ServerTestBase::TearDown();
+    }
+
+    virtual void SessionUp() {
+        BgpXmppMcast2ServerTestBase::SessionUp();
+
+        agent_za_.reset(new test::NetworkAgentMock(
+                &evm_, "agent-za", xs_z_->GetPort(), "127.0.0.7", "127.0.0.103"));
+        TASK_UTIL_EXPECT_TRUE(agent_za_->IsEstablished());
+        agent_zb_.reset(new test::NetworkAgentMock(
+                &evm_, "agent-zb", xs_z_->GetPort(), "127.0.0.8", "127.0.0.103"));
+        TASK_UTIL_EXPECT_TRUE(agent_zb_->IsEstablished());
+        agent_zc_.reset(new test::NetworkAgentMock(
+                &evm_, "agent-zc", xs_z_->GetPort(), "127.0.0.9", "127.0.0.103"));
+        TASK_UTIL_EXPECT_TRUE(agent_zc_->IsEstablished());
+    }
+
+    virtual void SessionDown() {
+        BgpXmppMcast2ServerTestBase::SessionDown();
+
+        agent_za_->SessionDown();
+        agent_zb_->SessionDown();
+        agent_zc_->SessionDown();
+        task_util::WaitForIdle();
+    }
+
+    virtual void Subscribe(const string net, int id) {
+        BgpXmppMcast2ServerTestBase::Subscribe(net, id);
+
+        agent_za_->McastSubscribe(net, id);
+        agent_zb_->McastSubscribe(net, id);
+        agent_zc_->McastSubscribe(net, id);
+        task_util::WaitForIdle();
+    }
+
+    boost::scoped_ptr<BgpServerTest> bs_z_;
+    XmppServer *xs_z_;
+    boost::scoped_ptr<BgpXmppChannelManagerMock> bcm_z_;
+    boost::scoped_ptr<test::NetworkAgentMock> agent_za_;
+    boost::scoped_ptr<test::NetworkAgentMock> agent_zb_;
+    boost::scoped_ptr<test::NetworkAgentMock> agent_zc_;
+};
+
+class BgpXmppMcast3ServerTest : public BgpXmppMcast3ServerTestBase {
+protected:
+    virtual void SetUp() {
+        BgpXmppMcast3ServerTestBase::SetUp();
+
+        Configure(config_tmpl3);
+        task_util::WaitForIdle();
+
+        BgpXmppMcast3ServerTestBase::SessionUp();
+        BgpXmppMcast3ServerTestBase::Subscribe("blue", 1);
+    }
+
+    virtual void TearDown() {
+        BgpXmppMcast3ServerTestBase::SessionDown();
+        BgpXmppMcast3ServerTestBase::TearDown();
+    }
+};
+
+TEST_F(BgpXmppMcast3ServerTest, SingleAgentPerServer) {
+    const char *mroute = "225.0.0.1,0.0.0.0";
+
+    // Add mcast route for agents xa, ya and za.
+    agent_xa_->AddMcastRoute("blue", mroute, "10.1.1.1", "10000-19999");
+    agent_ya_->AddMcastRoute("blue", mroute, "10.1.1.4", "40000-49999");
+    agent_za_->AddMcastRoute("blue", mroute, "10.1.1.7", "70000-79999");
+    task_util::WaitForIdle();
+
+    // Verify number of routes on all agents.
+    TASK_UTIL_EXPECT_EQ(1, agent_xa_->McastRouteCount());
+    TASK_UTIL_EXPECT_EQ(1, agent_ya_->McastRouteCount());
+    TASK_UTIL_EXPECT_EQ(1, agent_za_->McastRouteCount());
+
+    // Verify all OList elements on all agents.
+    VerifyOListElem(agent_xa_.get(), "blue", mroute, 2, "10.1.1.4");
+    VerifyOListElem(agent_xa_.get(), "blue", mroute, 2, "10.1.1.7");
+    VerifyOListElem(agent_ya_.get(), "blue", mroute, 1, "10.1.1.1");
+    VerifyOListElem(agent_za_.get(), "blue", mroute, 1, "10.1.1.1");
+
+    // Delete mcast route for all agents.
+    agent_xa_->DeleteMcastRoute("blue", mroute);
+    agent_ya_->DeleteMcastRoute("blue", mroute);
+    agent_za_->DeleteMcastRoute("blue", mroute);
+    task_util::WaitForIdle();
+
+    // Verify number of routes on all agents.
+    TASK_UTIL_EXPECT_EQ(0, agent_xa_->McastRouteCount());
+    TASK_UTIL_EXPECT_EQ(0, agent_ya_->McastRouteCount());
+    TASK_UTIL_EXPECT_EQ(0, agent_za_->McastRouteCount());
 };
 
 class TestEnvironment : public ::testing::Environment {
